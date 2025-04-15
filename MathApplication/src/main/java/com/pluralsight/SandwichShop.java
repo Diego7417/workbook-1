7246e360e202b318f8aa904b44fa03f984ba16d2
Diego7417 <Dangeles99@gmail.com>
2025-04-15 14:47:25 -0400

    Added Loaded option for the sandwich shop

--- MathApplication/src/main/java/com/pluralsight/SandwichShop.java
+++ MathApplication/src/main/java/com/pluralsight/SandwichShop.java
@@ -6,19 +6,45 @@ public class SandwichShop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double basePrice = 0.00;
-        double discount = 0.00;
+        double loadedPrice = 0.00;
 
-        System.out.println("Welcome to My Sandwich Shop");
-        System.out.print("Enter sandwich siz (1 - Regular, 2 - Large):");
+        System.out.println("Select sandwich size off menu");
+        System.out.println("1: Regular $5.45");
+        System.out.println("2: Large $8:95");
         int size = scanner.nextInt();
 
         if (size == 1){
             basePrice = 5.45;
+            loadedPrice = 1.00;
         } else if (size == 2){
             basePrice = 8.95;
+            loadedPrice = 1.75;
         } else {
             System.out.println("Invalid sandwich size, Please select 1 or 2");
+            return;
         }
+        System.out.println("Would you like your sandwich loaded? (yes/no):");
+        scanner.nextLine();
+        String loadedOption = scanner.nextLine();
+
+        if (loadedOption.equals("yes")){
+            basePrice += loadedPrice;
+        }
+
+        System.out.println("Enter your age:");
+        int age = scanner.nextInt();
+        double discount = 0.00;
+        if (age <= 17){
+            discount =0.10;
+
+        }else if (age >= 65);{
+            discount = 0.20;
+        }
+        double finalPrice = basePrice * (1 - discount);
+        System.out.println("Your total will be:$" + finalPrice);
+
+
+
 
     }
 }
